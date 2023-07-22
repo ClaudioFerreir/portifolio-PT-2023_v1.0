28745b128ac6639ea03e6f3aa2abe1f771c3f582
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-07-22 20:20:13 -0300

    fix: about content

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -4,7 +4,7 @@ const AboutContainer = () => {
       <h2>Sobre</h2>
       <p>
       Desenvolvedor Full Stack, com formação pela Le Wagon, pós graduando em Desenvolvimento e Análise de Programas e graduando em Ciência da Computação pelo Centro Universitário União das Américas Descomplica.
-      Aluno do Curso Desenvolvedor Full Stack Python pela Escola de Britânica de Artes Criativas e Tecnologia (EBAC). Engenheiro em Energias Renováveis pelo Instituto Politécnico de Bragança (IPB), Portugal.
+      Aluno do Curso Desenvolvedor Full Stack Python pela Escola de Britânica de Artes Criativas e Tecnologia (EBAC). Engenheiro em Energias Renováveis pelo Escola Superior de Tecnologia e Gestão - IPB, Portugal.
       </p>
     </section>
   )
